75e8850d1e3469b13012fa907f3b432b752193bb
Pame <geleivago@est.utn.ac.cr>
2023-11-30 14:12:17 -0600

    Se actualiza la Clase Pacientes
    Se actualiza el constructor de pacientes

--- src/Hospital/Pacientes.java
+++ src/Hospital/Pacientes.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Hospital;
 
-/**
- *
- * @author Student
- */
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Pacientes {
+    private String nombre;
+    private String cedula;
+    private LocalDate fecha;
+    private LocalTime horaLlegada;
+    private LocalTime horaSalida;
+    private String clasificacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHoraLlegada() {
+        return horaLlegada;
+    }
+
+    public LocalTime getHoraSalida() {
+        return horaSalida;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public Pacientes(String nombre, String cedula, LocalDate fecha, LocalTime horaLlegada, LocalTime horaSalida, String clasificacion) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.fecha = fecha;
+        this.horaLlegada = horaLlegada;
+        this.horaSalida = horaSalida;
+        this.clasificacion = clasificacion;
+    }
+    }
     
-}
+    
